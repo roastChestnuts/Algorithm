b504bdf083915573ea0766aaea7fbfd5886807e0
AhnByeongChan <like10495@gmail.com>
2022-10-27 01:02:11 +0900

    no message

--- 백준/브루트포스/10448.py
+++ 백준/브루트포스/10448.py
@@ -1,3 +1,24 @@
+##########시간 개선
+tri_num = [ (i*(i+1))//2 for i in range(1, 45) ]
+arr = [0] * 1001
+
+for i in tri_num:
+    for j in tri_num:
+        for k in tri_num:
+            if i + j + k <= 1000:
+                arr[i+j+k] = 1
+
+n = int(input())
+
+for i in range(n):
+    check = int(input())
+    answer = 0
+    
+    if arr[check] == 1:
+        answer = 1
+    print(answer)   
+
+###########기존코드
 tri_num = [ (i*(i+1))//2 for i in range(1, 45) ] # 대략 45정도의 삼각수라면 1000을 넘거나 근접하기때문에 이 이상은 구할 필요가 없다.
 
 n = int(input())
