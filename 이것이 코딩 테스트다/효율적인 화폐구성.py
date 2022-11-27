68a2cf4a025d70b4a092b9f299f15f2a86330080
AhnByeongChan <like10495@gmail.com>
2022-11-28 01:50:18 +0900

    no message

--- 이것이 코딩 테스트다/효율적인 화폐구성.py
+++ 이것이 코딩 테스트다/효율적인 화폐구성.py
@@ -3,11 +3,10 @@ coin_list = [0] * n
 for i in range(n):
     coin_list[i] = int(input())
 
-dp = [10001] * (n+1)
+dp = [10001] * (m+1)
 dp[0] = 0
 
 for coin in coin_list:
-    dp[coin] = 1
     for i in range(coin, m+1):
         if dp[i-coin] != 10001:
             dp[i] = min(dp[i-coin]+1, dp[i])
