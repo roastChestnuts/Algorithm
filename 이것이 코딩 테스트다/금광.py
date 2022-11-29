12f075e6a58899b17b0090545166fd31cfece335
AhnByeongChan <like10495@gmail.com>
2022-11-30 01:40:53 +0900

    no message

--- 이것이 코딩 테스트다/금광.py
+++ 이것이 코딩 테스트다/금광.py
@@ -26,3 +26,28 @@ for tc in range(int(input())) :
   for i in range(n) :
     result = max(result, dp[i][m - 1])
   print(result)
+
+
+#####################################
+for tc in range(int(input())) :
+  n, m = map(int, input().split())
+  arr = list(map(int, input().split()))
+  gold_list = []
+  
+  for i in range(0, n*m, m) :
+    gold_list.append(arr[i: i+m])
+
+  for j in range(1, m) :
+    for i in range(n) :
+      left_up, left_down, left = 0, 0, 0
+      if i != 0:
+        left_up = gold_list[i - 1][j - 1]
+      if i != (n - 1) :
+        left_down = gold_list[i + 1][j - 1]
+      left = gold_list[i][j - 1]
+      gold_list[i][j] = gold_list[i][j] + max(left_up, left_down, left)
+
+  result = 0
+  for i in range(n) :
+    result = max(result, gold_list[i][m - 1])
+  print(result)
