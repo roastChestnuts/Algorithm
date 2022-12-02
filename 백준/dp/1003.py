e3afe4e3703ce12b48e6df87d5b3d612d128b31a
AhnByeongChan <like10495@gmail.com>
2022-12-03 04:44:34 +0900

    no message

---
+++ 백준/dp/1003.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+dp = [[0]*2 for i in range(41)]
+
+dp[0][0] = 1
+dp[1][1] = 1
+
+for i in range(2, 41):
+  dp[i][0] = dp[i-1][0] + dp[i-2][0]
+  dp[i][1] = dp[i-1][1] + dp[i-2][1]
+
+for i in range(t):
+  n = int(input())
+  print(dp[n][0], dp[n][1])
