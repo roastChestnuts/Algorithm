ccb732fc29394f5b3456f77892126f3c0336acc5
AhnByeongChan <like10495@gmail.com>
2023-01-29 03:56:46 +0900

    230129_abc_백트래킹복습

--- 백준/백트래킹/15649.py
+++ 백준/백트래킹/15649.py
@@ -9,3 +9,21 @@ for i in ans:
     for j in i:
         print(j, end = ' ')
     print()
+
+#########################################
+#백트래킹 이용
+
+n, m = map(int, input().split())
+list = [i for i in range(1, n+1)]
+ans = []
+
+def func():
+    if len(ans) == m:
+        print(*ans)
+        return
+    for i in list:
+        if i not in ans:
+            ans.append(i)
+            func()
+            ans.pop()
+func()          
