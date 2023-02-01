c5540da24440cdb9553d939b7bb504efc8d89517
AhnByeongChan <like10495@gmail.com>
2023-02-02 02:13:21 +0900

    230202_abc_1449다시풀이

--- 백준/그리디/1449.py
+++ 백준/그리디/1449.py
@@ -11,3 +11,18 @@ for i in range(1,n):
         count +=1
         l_ = l-1
 print(count)        
+###########################230202 다시
+import sys
+input = sys.stdin.readline
+
+n, l = map(int, input().split())
+pipe = list(map(int, input().split()))
+pipe.sort()
+count = 1
+start = pipe[0]
+
+for i in range(1, n):
+    if pipe[i]-start > l-1:
+        count += 1
+        start = pipe[i]
+print(count)
