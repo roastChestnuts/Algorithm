5b8297f3ecbfb15b5fcfbb6eed46906bbef949cc
AhnByeongChan <like10495@gmail.com>
2023-02-02 01:10:26 +0900

    230202_abc_4796다시풀이

--- 백준/그리디/4796.py
+++ 백준/그리디/4796.py
@@ -14,3 +14,18 @@ while True:
       day += (v%p)
     print(f'Case {count}: {day}')
     count +=1
+##########################230202 다시
+count = 1
+while True:
+    l, p, v = map(int, input().split())
+    if l+p+v == 0:
+        break
+    day = (v//p)*l
+
+    if v%p <= l:
+        day += v%p
+    else:
+        day += l
+    print(f"Case {count}: {day}")
+    count += 1
+        
