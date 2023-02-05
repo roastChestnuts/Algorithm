5e3eb4f9fd8efec7d5086514ff3c7e59da66a393
AhnByeongChan <like10495@gmail.com>
2023-02-06 03:55:00 +0900

    230206_abc_소수찾기(완탐) - 다시!

---
+++ 프로그래머스/고득점 kit/완전탐색/소수찾기.py
@@ -0,0 +1,15 @@
+from itertools import permutations
+
+def solution(n):
+    prime = set()
+    #모든 숫자 조합을 만든다
+    for i in range(len(n)):
+        prime |= set(map(int, map("".join, permutations(list(n), i + 1))))
+
+    #소수가 아닌 수를 제외한다.
+    prime -= set(range(0, 2))
+    for i in range(2, int(max(prime) ** 0.5) + 1):
+        prime -= set(range(i * 2, max(prime) + 1, i))
+    return len(prime)
+
+print(solution("113"))
